b10072761ffd5b47239440cb5f51484a08aeeaf5
kanikashreesivakumar <kanikashreesivakumar16@gmail.com>
2025-03-19 12:34:55 +0530

    kani loosu

--- backend/app.py
+++ backend/app.py
@@ -64,4 +64,5 @@ def get_thoughts(user_id):
 
 # Run Flask App
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
